52c0bbe515ac378f57a4d1f338c3d35de2e5d2a2
Filip Dupanović <filip.dupanovic@gmail.com>
2016-11-17 10:44:02 +0100

    feat(plugin): Add support for committing modules after build

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+const debug = require('debug')('screeps-webpack-plugin')
+const path = require('path')
+
+const ScreepsModules = require('screeps-modules')
+
+// Events.
+const COLLECT_MODULES = 'screeps-webpack-plugin-collect-modules'
+const CONFIG_CLIENT = 'screeps-webpack-plugin-configure-client'
+const BEFORE_COMMIT = 'screeps-webpack-plugin-before-commit'
+const AFTER_COMMIT = 'screeps-webpack-plugin-after-commit'
 
 class ScreepsWebpackPluginError extends Error {
   constructor (msg) {
     super(msg)
-    this.foo = 'bar'
     this.name = 'ScreepsWebpackPluginError'
   }
 }
 
 class ScreepsWebpackPlugin {
+  constructor (options) {
+    this.options = options
+  }
+
   apply (compiler) {
     compiler.plugin('compilation', (compilation) => {
       if (compiler.options.target !== 'node') {
@@ -15,8 +28,110 @@ class ScreepsWebpackPlugin {
 
         return compilation.errors.push(err)
       }
+
+      this.registerHandlers(compilation)
+    })
+
+    compiler.plugin('after-emit', (compilation, cb) => {
+      Promise.resolve()
+        .then(() => {
+          return new Promise((resolve, reject) => {
+            const initial = {
+              modules: {},
+              plugin: this,
+              compilation
+            }
+
+            compilation.applyPluginsAsyncWaterfall(COLLECT_MODULES, initial, (err, {modules}) => {
+              if (err) {
+                debug('Error while collecting modules', err.stack)
+
+                return reject(err)
+              } else {
+                resolve(modules)
+              }
+            })
+          })
+        })
+      .then((modules) => {
+        const client = compilation.applyPluginsWaterfall(CONFIG_CLIENT, null, this)
+        const {branch} = this.options
+
+        compilation.applyPlugins(BEFORE_COMMIT, branch, modules)
+
+        return client.commit(branch, modules)
+          .then((body) => {
+            compilation.applyPlugins(AFTER_COMMIT, body)
+          })
+          .catch((body) => {
+            throw new Error(body)
+          })
+      })
+      .then(cb)
+      .catch((err) => {
+        compilation.errors.push(new ScreepsWebpackPluginError(err.stack))
+
+        cb()
+      })
     })
   }
+
+  registerHandlers (compilation) {
+    compilation.plugin(COLLECT_MODULES, this.collectModules)
+    compilation.plugin(CONFIG_CLIENT, this.configureClient)
+  }
+
+  collectModules ({modules: initial, plugin, compilation}, cb) {
+    const chunks = compilation.getStats().toJson().chunks
+    const outputPath = compilation.options.output.path
+    const files = []
+
+    for (const chunk of chunks) {
+      for (const file of chunk.files) {
+        files.push(path.resolve(outputPath, file))
+      }
+    }
+
+    const outputFileSystem = compilation.compiler.outputFileSystem
+    const promises = []
+
+    for (const file of files) {
+      promises.push(new Promise((resolve, reject) => {
+        outputFileSystem.readFile(file, 'utf-8', (err, data) => {
+          if (err) {
+            return reject(err)
+          }
+
+          const moduleName = path.basename(file)
+
+          resolve({[moduleName]: data})
+        })
+      }))
+    }
+
+    Promise.all(promises)
+      .then((files) => {
+        const modules = files.reduce((modules, file) => {
+          Object.assign(modules, file)
+
+          return modules
+        }, initial || {})
+
+        cb(null, {modules, plugin, compilation})
+      })
+      .catch(cb)
+  }
+
+  configureClient (initial, plugin) {
+    return new ScreepsModules(plugin.options)
+  }
 }
+
+Object.assign(ScreepsWebpackPlugin, {
+  COLLECT_MODULES,
+  CONFIG_CLIENT,
+  BEFORE_COMMIT,
+  AFTER_COMMIT
+})
 
 module.exports = ScreepsWebpackPlugin
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import MemoryFs from 'memory-fs'
+import ScreepsModules from 'screeps-modules'
 import test from 'ava'
 import webpack from 'webpack'
+import Compilation from 'webpack/lib/Compilation'
 
 import ScreepsWebpackPlugin from './index'
 
@@ -9,15 +11,16 @@ const debug = require('debug')('screeps-webpack-plugin')
 function compile (options) {
   const compiler = webpack(Object.assign({
     target: 'node',
-    entry: [
-      'index.js'
-    ],
+    entry: {
+      main: ['index.js'],
+      etc: ['foo.js', 'bar.js']
+    },
     resolve: {
       modules: ['./fixtures']
     },
     output: {
       path: '/',
-      filename: 'main'
+      filename: '[name]'
     },
     plugins: []
   }, options))
@@ -37,6 +40,16 @@ function compile (options) {
     })
   })
 }
+
+const plugin = (name, ...args) => (
+  new (class {
+    apply (compiler) {
+      compiler.plugin('compilation', (compilation) => {
+        compilation.plugin(name, ...args)
+      })
+    }
+  })()
+)
 
 const checkError = (t, err, ...checks) => {
   t.is(err.name, 'ScreepsWebpackPluginError')
@@ -73,6 +86,68 @@ test(`Test requires target 'node'`, async t => {
   } catch ([e]) {
     checkError(t, e, 'target', 'node')
   }
+})
+
+test('Test commit', async t => {
+  t.plan(10)
+
+  const collectModules = plugin('screeps-webpack-plugin-collect-modules',
+    ({modules, plugin, compilation}, cb) => {
+      t.deepEqual(Object.keys(modules), ['etc', 'main'])
+      t.truthy(modules.main.match(/foobar/))
+      t.truthy(modules.etc.match(/foobar/))
+
+      t.true(plugin instanceof ScreepsWebpackPlugin)
+      t.true(compilation instanceof Compilation)
+
+      modules.quux = 'norf'
+
+      cb(null, {modules, plugin, compilation})
+    }
+  )
+
+  const configureClient = plugin('screeps-webpack-plugin-configure-client',
+    (client, plugin) => {
+      t.true(client instanceof ScreepsModules)
+      t.true(plugin instanceof ScreepsWebpackPlugin)
+
+      client.commit = (...args) => {
+        return Promise.resolve('foobar')
+      }
+
+      return client
+    }
+  )
+
+  const beforeCommit = plugin('screeps-webpack-plugin-before-commit',
+    (branch, modules) => {
+      t.is(branch, 'test')
+      t.is(modules.quux, 'norf')
+    }
+  )
+
+  const afterCommit = plugin('screeps-webpack-plugin-after-commit',
+    (body) => {
+      t.is(body, 'foobar')
+    }
+  )
+
+  await compile({
+    plugins: [
+      new ScreepsWebpackPlugin({
+        branch: 'test',
+        email: 'foobar',
+        password: 'barbaz',
+        token: 'quuxnorf',
+        serverUrl: 'https://foo.com',
+        gzip: true
+      }),
+      collectModules,
+      configureClient,
+      beforeCommit,
+      afterCommit
+    ]
+  })
 })
 
 test('Test constructor', t => {
